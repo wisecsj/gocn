863664a3d76e6623139235b30f0085a592dcf338
wisej <wisecsj@gmail.com>
2019-03-06 20:39:07 +0800

    file server

--- cn/server.go
+++ cn/server.go
@@ -1,17 +1,19 @@
-// 很意外，golang标准库居然实现了这么一个文件服务器的Handler
-// 其实自己实现一个简易的也并不难，也就是实现一个Handler，然后
-// 把对应的文件字节流写进socket.不过标准库的FileServer还实现
-// 了Range（从而支持客户端进行并行下载和断点续传）。并且访问跟目录
-// 的时候，还会很方便地把文件列表用a标签展示出来
-package main
-
+package cn
 
 import (
 	"log"
 	"net/http"
 )
 
-func main() {
+const rootPath string = "/home/wisej/桌面"
+
+// LaunchFileServer .
+// 很意外，golang标准库居然实现了这么一个文件服务器的Handler
+// 其实自己实现一个简易的也并不难，也就是实现一个Handler，然后
+// 把对应的文件字节流写进socket.不过标准库的FileServer还实现
+// 了Range（从而支持客户端进行并行下载和断点续传）。并且访问跟目录
+// 的时候，还会很方便地把文件列表用a标签展示出来
+func LaunchFileServer() {
 	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/home/wisej/桌面"))))
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir(rootPath))))
 }
